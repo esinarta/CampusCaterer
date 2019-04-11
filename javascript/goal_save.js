e865f5383d1d10268e5bc352b926ea1f42396ada
john lim <johnlim0612@gmail.com>
2019-04-11 10:47:11 -0700

    Revert to fix

--- javascript/goal_save.js
+++ javascript/goal_save.js
@@ -32,7 +32,7 @@ if (document.getElementById("cb1").checked == true) {
     guide1: weight
     });
 
-} else if (document.getElementById("cb1").checked == false) {
+} if (document.getElementById("cb1").checked == false) {
     firebase.database().ref("users/" + firebase.auth().currentUser.uid + "/goal1").set({
     goal1: null
     });
@@ -54,7 +54,7 @@ if (document.getElementById("cb2").checked == true) {
     });
 
 
-} else if (document.getElementById("cb2").checked == false) {
+} if (document.getElementById("cb2").checked == false) {
     firebase.database().ref("users/" + firebase.auth().currentUser.uid + "/goal2").set({
     goal2: null
     });
@@ -119,6 +119,7 @@ if (document.getElementById("cb5").checked == true) {
     guide5: eco
     });
 
+
 } else if (document.getElementById("cb5").checked == false){
     firebase.database().ref("users/" + firebase.auth().currentUser.uid + "/goal5").set({
     goal5: null
@@ -129,6 +130,6 @@ if (document.getElementById("cb5").checked == true) {
     });
 
 }
-setTimeout(function(){ window.location.href="results.html" }, 800);
+setTimeout(function(){ window.location.href="results.html" }, 250);
 }
       
--- readme.txt
+++
@@ -1,8 +0,0 @@
-Team 27 - John, Estevan, Amar
-Campus Caterer
-
-Folders:
--assets: photos and backgrounds used
--javascript: .js files for javascript code
--html: HTML files
--css: CSS files
